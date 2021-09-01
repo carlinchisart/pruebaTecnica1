3879b1f39861d808263cefe9710732ae4a167cf5
carlos perez <carlinchisart@gmail.com>
2021-08-31 20:54:30 -0500

    se corrigen factoris

--- database/factories/CamionFactory.php
+++ database/factories/CamionFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use App\Models\Camion;
+use App\Models\Finca;
+use App\Models\Repartidor;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CamionFactory extends Factory
@@ -22,7 +24,8 @@ class CamionFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'finca_id' => Finca::inRandomOrder()->first()->id,
+            'repartidor_id' => Repartidor::inRandomOrder()->first()->id
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Camion;
+use App\Models\Finca;
+use App\Models\Lote;
+use App\Models\LoteManifiesto;
+use App\Models\Manifiesto;
+use App\Models\Repartidor;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +19,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Finca::factory(10)->create();
+        Repartidor::factory(50)->create();
+        Camion::factory(20)->create();
+        Lote::factory(200)->create();
+        Manifiesto::factory(500)->create();
+        LoteManifiesto::factory(2000)->create();
     }
 }
