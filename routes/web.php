dba8a1db76565e5ee43240a9014e824a97ef5936
carlos perez <carlinchisart@gmail.com>
2021-08-31 19:15:53 -0500

    se crea ruta para mostrar la vista

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Nomina;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('nomina',[Nomina::class,'index'])->name('nomina');
+Route::get('manifiestos/{camion_id}',[Nomina::class,'manifiestos'])->name('manifiestos');
